c67d6cae32b7cb082793eb6b7206872665d5d636
Vidar Magnusson <git@vidarmagnusson.com>
2020-09-25 16:33:02 +0200

    Finished? Lab2

---
+++ cmake-build-debug/CMakeFiles/EDA093.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EDA093.dir/lsh.c.o"
+  "CMakeFiles/EDA093.dir/parse.c.o"
+  "EDA093"
+  "EDA093.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/EDA093.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- mount/lab2/pintos/src/threads/thread.c
+++ mount/lab2/pintos/src/threads/thread.c
@@ -552,8 +552,7 @@ thread_schedule_tail (struct thread *prev)
 
    It's not safe to call printf() until thread_schedule_tail()
    has completed. */
-static void
-schedule (void) 
+static void schedule (void)
 {
   struct thread *cur = running_thread ();
   struct thread *next = next_thread_to_run ();
@@ -581,7 +580,7 @@ allocate_tid (void)
 
   return tid;
 }
-
+
 /* Offset of `stack' member within `struct thread'.
    Used by switch.S, which can't figure it out on its own. */
 uint32_t thread_stack_ofs = offsetof (struct thread, stack);
